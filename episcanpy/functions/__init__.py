9745d257ca126ba50f8f520205ef085da0798926
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-02-05 17:20:22 +0100

    Create __init__.py

---
+++ episcanpy/functions/__init__.py
@@ -0,0 +1,35 @@
+"""\
+Plotting API
+============
+.. automodule:: episcanpy
+
+.. note::
+
+    I am not sure what to write here. But please enjoy if it get printed somewhere!
+    
+Generic
+-------
+
+.. autosummary::
+   :toctree: .
+   
+   pl.scatter
+   pl.heatmap
+   pl.dotplot
+   pl.violin
+   pl.stacked_violin
+   pl.matrixplot
+   pl.clustermap
+   pl.ranking
+   
+Preprocessing
+-------------
+
+Methods for visualizing quality control and results of preprocessing functions.
+
+.. autosummary::
+   :toctree: .
+   
+   pl.highest_expr_genes
+   pl.filter_genes_dispersion
+   pl.highly_variable_genes
