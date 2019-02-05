00918bcc6e36662beb04e78d879be0b256b58daf
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-02-05 22:29:25 +0100

    Update load.py

--- episcanpy/api/load.py
+++ episcanpy/api/load.py
@@ -1,2 +1,2 @@
-from ..functions.extract_meth import extract_CG, extract_CH
-from ..functions.load_features import load_features
+from ..load._extract import extract_CG, extract_CH
+from ..load._load_features import load_features
