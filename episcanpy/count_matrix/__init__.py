dd017ba9d4cfa2e25821c28f0ce9ebaffbe0aac2
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-02-11 16:44:22 +0100

    Update and rename episcanpy/load/__init__.py to episcanpy/count_matrix/__init__.py

---
+++ episcanpy/count_matrix/__init__.py
@@ -0,0 +1,2 @@
+from ..count_matrix._read_meth_file import read_methylation_file
+from ..count_matrix._extract import extract_CG, extract_CH
--- episcanpy/load/__init__.py
+++
@@ -1,2 +0,0 @@
-from ..load._read_meth_file import read_methylation_file
-from ..load._extract import extract_CG, extract_CH
