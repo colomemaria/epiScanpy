81ecae99ed74f156542bc5ccd2823befe73bd667
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-02-11 16:45:30 +0100

    Update ct.py

--- episcanpy/api/ct.py
+++ episcanpy/api/ct.py
@@ -1,2 +1,2 @@
 from ..functions._load_features import load_features, make_windows
-from ..load._read_meth_file import read_methylation_file
+from ..count_matrix._read_meth_file import read_methylation_file
