265a166f144b10e79a4508870531d9845d0cc785
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-05-02 13:41:48 +0200

    Update _recipe.py

--- episcanpy/preprocessing/_recipe.py
+++ episcanpy/preprocessing/_recipe.py
@@ -1,7 +1,9 @@
 import scanpy.api as sc
 import anndata as ad
 
-def lazy(adata, pp_pca=True, copy=False):
+def lazy(adata, pp_pca=True, nb_pcs=50, n_neighbors=15, perplexity=30, 
+         method='umap', metric='euclidean', min_dist=0.5, spread=1.0, 
+         n_components=2, copy=False):
     '''
     Automatically computes PCA coordinates, loadings and variance decomposition, a neighborhood graph of observations,
     t-distributed stochastic neighborhood embedding (tSNE) Uniform Manifold Approximation and Projection (UMAP)
@@ -12,8 +14,36 @@ def lazy(adata, pp_pca=True, copy=False):
         Annotated data matrix.
     pp_pca :  `bool` (default: `True`)
         Computes PCA coordinates before the neighborhood graph
+    nb_pcs : `int` (default: 50)
+        Number of principal component computed for PCA (and therefore neighbors, tsne and umap)
+    n_neighbors : `int` (default: 15)
+        The size of local neighborhood (in terms of number of neighboring data points) used for manifold approximation.
+        Larger values result in more global views of the manifold, while smaller values result in more local data being
+        preserved. In general values should be in the range 2 to 100.
+    method : `str` (default: ‘umap’)
+        Use ‘umap’ or ‘gauss’, kernel for computing connectivities. Gives very similar results.
+    metric : `str` (default: ‘euclidean’)
+        A known metric’s name or a callable that returns a distance.
+    perplexity : `int` (default: 30)
+        The perplexity is related to the number of nearest neighbors that is used in other manifold learning algorithms.
+        Larger datasets usually require a larger perplexity. Consider selecting a value between 5 and 50. 
+    min_dist : `float` (default: 0.5)
+        The effective minimum distance between embedded points. Smaller values will result in a more clustered/clumped
+        embedding where nearby points on the manifold are drawn closer together, while larger values will result on a
+        more even dispersal of points.
+    spread : `float` (default: 1.0)
+        The effective scale of embedded points. In combination with `min_dist` this determines how clustered/clumped
+        the embedded points are.
+    n_components : `int` (default: 2)
+        The number of dimensions of the `UMAP` embedding.
     copy : `bool` (default: `False`)
         Return a copy instead of writing to adata.
+    
+    method='umap',
+    metric='euclidean',
+    min_dist=0.5,
+    spread=1.0, 
+    n_components=2
     
     
     Returns
@@ -40,12 +70,12 @@ def lazy(adata, pp_pca=True, copy=False):
         adata
 
     if pp_pca:
-        sc.pp.pca(adata)
+        sc.pp.pca(adata, n_comps=nb_pcs)
     
-    sc.pp.neighbors(adata)
-    sc.tl.pca(adata)
-    sc.tl.tsne(adata)
-    sc.tl.umap(adata)
+    sc.pp.neighbors(adata,  n_neighbors=n_neighbors, n_pcs=nb_pcs, method=method, metric=metric)
+    sc.tl.pca(adata, n_comps=nb_pcs)
+    sc.tl.tsne(adata, n_pcs=nb_pcs, perplexity=perplexity)
+    sc.tl.umap(adata, min_dist, spread, n_components)
     
     if copy:
         return(adata)
