163a01d9e4a59a785c291cd067bdc1f3ce3c17bf
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-05-06 15:53:21 +0200

    add bad_atac_sim

--- episcanpy/datasets/__init__.py
+++ episcanpy/datasets/__init__.py
@@ -2,7 +2,7 @@ import anndata as ad
 import scanpy.api as sc
 from anndata import AnnData
 
-def atac_blobs(n_variables=11, n_centers=5, cluster_std=1.0, n_observations=640) -> AnnData:
+def normal_blobs(n_variables=11, n_centers=5, cluster_std=1.0, n_observations=640) -> AnnData:
     """Gaussian Blobs, ATAC.
     Parameters
     ----------
@@ -27,6 +27,46 @@ def atac_blobs(n_variables=11, n_centers=5, cluster_std=1.0, n_observations=640)
                                        cluster_std=cluster_std,
                                        random_state=0)
     return AnnData(X, obs={'blobs': y.astype(str)})
+
+from anndata import AnnData
+import numpy as np
+import sklearn.datasets
+
+def bad_atac_sim(n_variables=5000, n_centers=4, cluster_std=2.0, n_observations=3000, dropouts=None)
+    """
+    Generate isotropic Gaussian blobs from sklearn 'make_blobs' for clustering. The data are then binarised
+    and we account for dropout. 
+    n_variables = nb of features for the simulation
+    n_centers = number of predicted clusters
+    cluster_std = standard deviation of the clusters
+    n_observations = number of cells
+    dropouts = amount of dropout (between 0 and 1) default=0.5
+    
+    return a not annotated adata mastrix
+    """
+    
+    X, y = sklearn.datasets.make_blobs(n_samples=n_observations,
+                                       n_features=n_variables,
+                                       centers=n_centers,
+                                       center_box = (0, 1),
+                                       cluster_std=cluster_std,
+                                       random_state=0)
+    adata = AnnData(X, obs={'blobs': y.astype(str)})
+
+    if dropout=None or dropout = 0.5:
+        threshold = np.median(adata.X)  
+    elif dropout>=1:
+        threshold=np.amax(adata.X)
+    elif dropout>=0.5:
+        threshold=np.amax(adata.X)*dropout
+    elif dropout<=0:
+        threshold = 0
+    else:
+        threshold = np.median(adata.X)*dropout
+    
+    adata.X = (adata.X > threshold).astype(np.int_)
+
+    return (adata)
 
 def meth_blobs(n_variables=11, n_centers=5, cluster_std=1.0, n_observations=640) -> AnnData:
     """Gaussian Blobs, methylation levels.
