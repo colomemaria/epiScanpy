10f3f5d2d6fe27bd32d306535e5692fd3d1c3c01
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-05-07 15:46:18 +0200

    Update __init__.py

--- episcanpy/tools/__init__.py
+++ episcanpy/tools/__init__.py
@@ -2,3 +2,4 @@ from ..tools._impute_meth import readandimputematrix
 from ..tools._load_matrix import read_ATAC
 from ..tools._features_selection import rank_features
 from ..tools._silhouette import silhouette
+from ..tools._recipe import lazy
