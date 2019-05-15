bc0b3f4094b7d5e781a0642f869c4b56caa1c5cb
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-05-15 08:25:04 +0200

    Update ct.py

--- episcanpy/api/ct.py
+++ episcanpy/api/ct.py
@@ -1,3 +1,3 @@
-from ..count_matrix._features import load_features, make_windows, size_feature_norm, plot_size_features
+from ..count_matrix._features import load_features, make_windows, size_feature_norm, plot_size_features, name
 from ..count_matrix._load_input_file import read_cyt_summary
 from ..count_matrix._bld_met_mtx import build_count_mtx
