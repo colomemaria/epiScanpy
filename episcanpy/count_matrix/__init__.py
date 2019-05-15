6f51583e5009777af0588f8323ad2f4ad0e21e7b
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-05-15 16:28:02 +0200

    Update __init__.py

--- episcanpy/count_matrix/__init__.py
+++ episcanpy/count_matrix/__init__.py
@@ -1,6 +1,7 @@
-from ..count_matrix._features import load_features, make_windows, size_feature_norm, plot_size_features, name
+from ..count_matrix._features import load_features, make_windows, size_feature_norm, plot_size_features, name_features
 from ..count_matrix._load_input_file import read_cyt_summary
 from ..count_matrix._bld_met_mtx import build_count_mtx
+from ..count_matrix._atac_mtx import bld_atac_mtx, save_sparse_mtx
 #from ..count_matrix._bld_met_mtx import methylation_level, write_not_sparse_meth, extract_methylation, extract_feature_names
 #from ..count_matrix._bld_met_mtx import read_meth_fileCG, read_meth_fileCH, read_meth_file, filter_and_average_features
 #from ..count_matrix._bld_met_mtx import filter_and_average_features_chrm, prep_methlevels, write_list, write_methlevel
