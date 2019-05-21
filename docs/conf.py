1e663f5aad03400fe6257090ad55598ee8fc313e
Anna Danese <anna.danese@helmholtz-muenchen.de>
2019-05-21 02:49:04 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,8 +33,8 @@ logger = logging.getLogger(__name__)
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-#needs_sphinx = '1.0'
-needs_sphinx = '1.7'  # autosummary bugfix
+#needs_sphinx = '1.7' # autosummary bugfix
+needs_sphinx = '3.0'  
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
