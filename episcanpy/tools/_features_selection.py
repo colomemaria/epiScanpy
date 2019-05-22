56d46a2e831a97dde772a75677e312a0b6f359db
DaneseAnna <anna.danese@helmholtz-muenchen.de>
2019-05-22 14:42:36 +0200

    Update _features_selection.py

--- episcanpy/tools/_features_selection.py
+++ episcanpy/tools/_features_selection.py
@@ -1,4 +1,4 @@
-import scanpy.api as sc
+from scanpy.api.tl import rank_genes_groups
 import warnings
 
 def rank_features(adata, groupby, omic=None, use_raw=True, groups='all', reference='rest', n_genes=100,
