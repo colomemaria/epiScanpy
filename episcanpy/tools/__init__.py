ca59fc0cbeea1ddf8157733862ef60d279d2a63f
DaneseAnna <anna.danese@helmholtz-muenchen.de>
2019-10-27 23:14:26 +0100

    import top_feature_genes correctly

--- episcanpy/tools/__init__.py
+++ episcanpy/tools/__init__.py
@@ -3,7 +3,7 @@ from ..tools._features_selection import rank_features
 from ..tools._silhouette import silhouette
 from ..tools._recipe import lazy
 from ..tools._cell_id import load_markers, identify_cluster
-#from ..tools._find_genes import top_feature_genes, find_genes
+from ..tools._top_feature_genes import top_feature_genes
 from ..tools._find_genes import find_genes
 from ..tools._scanpy_fct import pca, diffmap, draw_graph, tsne, umap
 from ..tools._scanpy_fct import louvain, leiden
--- episcanpy/tools/_find_genes.py
+++ episcanpy/tools/_find_genes.py
@@ -8,57 +8,6 @@ import matplotlib.axes as pltax
 import pandas as pd
 import pyranges as pr
 
-
-def top_feature_genes(adata, gtf_file, extension=5000):
-    
-    # extract_top_markers
-    #print(adata.uns['rank_genes_groups'].keys())
-    windows = [list(w) for w in adata.uns['rank_genes_groups']['names'].tolist()]
-    windows_all = []
-    for w in windows:
-        windows_all += w
-
-    # load the gtf file
-    with open(gtf_file) as f:
-        gtf_raw = f.readlines()
-    gtf = []
-    for line in gtf_raw:
-        if line[0] != '#':
-            gtf.append(line[:-2].split('\t'))
-    del gtf_raw
-    
-    gtf_dic = {}
-    for line in gtf:
-        if line[0] not in gtf_dic.keys():
-            gtf_dic[line[0]] = [line]
-        else:
-            gtf_dic[line[0]].append(line)
-    del gtf
-
-    markers = []
-    for w in windows_all:
-        curr_m = []
-        w2 = w.split('_')
-        chrom = w2[0][3:]
-        w2 = [int(x) for x in w2[1:]]
-        for gene in gtf_dic[chrom]:
-            start = int(gene[3])-extension
-            end = int(gene[4])+extension
-            if (w2[0] < start < w2[1]) or (w2[0] < end < w2[1]):
-                gene_name = gene[-1]
-                for n in gene[-1].split(';'):
-                    if 'gene_name' in n:
-                        gene_name = n
-                curr_m.append([w, gene_name])
-        if curr_m != []:
-            markers += curr_m
-    markers = [list(x) for x in set(tuple(x) for x in markers)]
-    markers_dict={}
-    for n in markers:
-        markers_dict[n[1].split(' "')[1][:-1]] = n[0]
-    return(markers_dict)
-
-
 def find_genes(adata, gtf_file_name, path='', extension=5000,
                key_added='gene_name', feature_coordinates=None, copy=True):
     """
