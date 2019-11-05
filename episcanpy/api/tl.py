adcfb33df149789fa671d57ef7fd0991cce09034
DaneseAnna <anna.danese@helmholtz-muenchen.de>
2019-11-05 15:08:18 +0100

    find genes function

--- episcanpy/api/tl.py
+++ episcanpy/api/tl.py
@@ -5,7 +5,7 @@ from ..tools._recipe import lazy
 from ..tools._cell_id import load_markers, identify_cluster
 
 from ..tools._top_feature_genes import top_feature_genes
-#from ..tools._find_genes import find_genes
+from ..tools._find_genes import find_genes
 #from ..tools._find_genes2 import find_genes_in_features
 
 from ..tools._scanpy_fct import pca, diffmap, draw_graph, tsne, umap
