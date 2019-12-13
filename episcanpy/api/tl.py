2b5f61cdda486fcb518a9cf3d11b240c6b7b0e45
DaneseAnna <anna.danese@helmholtz-muenchen.de>
2019-12-13 11:19:32 +0100

    update init files to load the new quality control files

--- episcanpy/api/tl.py
+++ episcanpy/api/tl.py
@@ -4,8 +4,8 @@ from ..tools._silhouette import silhouette
 from ..tools._recipe import lazy
 from ..tools._cell_id import load_markers, identify_cluster
 
-from ..tools._top_feature_genes import top_feature_genes
-#from ..tools._find_genes import find_genes
+from ..tools._top_feature_genes import top_feature_genes, var_features_to_genes
+from ..tools._find_genes import find_genes
 #from ..tools._find_genes2 import find_genes_in_features
 
 from ..tools._scanpy_fct import pca, diffmap, draw_graph, tsne, umap
