2d8264602b838215920650e326fd64d3f6ff416e
DaneseAnna <anna.danese@helmholtz-muenchen.de>
2019-12-13 11:55:17 +0100

    tmp remove find_genes fct from api

--- episcanpy/tools/__init__.py
+++ episcanpy/tools/__init__.py
@@ -8,4 +8,4 @@ from ..tools._top_feature_genes import top_feature_genes, var_features_to_genes
 from ..tools._scanpy_fct import pca, diffmap, draw_graph, tsne, umap
 from ..tools._scanpy_fct import louvain, leiden
 from ..tools._scanpy_fct import dendogram
-from ..tools._find_genes import find_genes
+#from ..tools._find_genes import find_genes
