34e9eea001ee57069c3ceb8fcd3a7785cc2c9c09
DaneseAnna <anna.danese@helmholtz-muenchen.de>
2019-12-19 15:47:49 +0100

    load_bedtool_matrix back compatibility

--- episcanpy/preprocessing/_load_atac.py
+++ episcanpy/preprocessing/_load_atac.py
@@ -43,6 +43,44 @@ def load_peak_matrix(matrix_file, path=''):
     
     return(adata)
 
+def load_bedtool_matrix(matrix_file, path=''):
+    
+    """
+    Deprecated - Use load_atac_matrix instead
+
+    Load existing peak x cell matrix into an anndata object.
+    Input existing peak matrix in the format as in Cusanovich et al. 2018 
+    First row of the matrix file is considered the header (chr start end annot cell1...cellN)
+    Paramters
+    ---------
+    matrix_file: txt or tsv file with the matrix in shape peaks x cells  
+    
+    Return
+    ------
+    AnnData  
+    """
+    warn('Deprecated - Use load_atac_matrix instead')
+    peak_name = []
+    cell_matrix = [] 
+    with open(path+matrix_file) as f:
+        head = f.readline().rstrip("\n").split('\t')
+        for line in f:
+            line = line.rstrip("\n").split('\t')
+            peak_name.append(line[3]) # for some reason it has rownames
+            line2 = []
+            for x in line[5:]:
+                line2.append(int(x))
+            cell_matrix.append(line2)
+    cell_names = head[4:]
+    
+    cell_matrix=csc_matrix(np.matrix(cell_matrix)).transpose()   
+    adata = ad.AnnData(cell_matrix,
+                   obs=pd.DataFrame(index=cell_names),
+                   var=pd.DataFrame(index=peak_name))
+    adata.uns['omic'] = 'ATAC'
+    
+    return(adata)
+
 def load_atac_matrix(matrix_file, path='', compression=None):
     """
 
