f1c214f20ade1754276f0bfb85ed6a00e6768977
DaneseAnna <anna.danese@helmholtz-muenchen.de>
2019-12-20 07:53:10 +0100

    init load_atac_matrix

--- episcanpy/preprocessing/__init__.py
+++ episcanpy/preprocessing/__init__.py
@@ -7,7 +7,7 @@ from ..preprocessing._quality_control import select_var_feature, cal_var, variab
 from ..preprocessing._recipe import lazy
 from ..preprocessing._metadata import load_metadata
 from ..preprocessing._load_matrix import read_ATAC_10x
-from ..preprocessing._load_atac import load_peak_matrix, load_bedtool_matrix
+from ..preprocessing._load_atac import load_peak_matrix, load_bedtool_matrix, load_atac_matrix
 from ..preprocessing._scanpy_fct import filter_cells, filter_features, normalize_total #, scale
 from ..preprocessing._scanpy_fct import pca, normalize_per_cell, regress_out, subsample, downsample_counts 
 from ..preprocessing._neighbors import neighbors
