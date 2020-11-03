0508c27be4061facc5800eb49473e616b5159b8a
kridsadakorn.cha@gmail.com <kridsadakorn.cha@gmail.com>
2020-11-03 02:26:57 +0100

    all parallel function to build matrix from BED file

--- episcanpy/count_matrix/__init__.py
+++ episcanpy/count_matrix/__init__.py
@@ -3,7 +3,7 @@ from ..count_matrix._load_input_file import read_cyt_summary
 from ..count_matrix._bld_met_mtx import build_count_mtx
 from ..count_matrix._atac_mtx import bld_atac_mtx, save_sparse_mtx
 from ..count_matrix._load_met_ct_mtx import load_met_noimput
-from ..count_matrix._bld_atac_mtx2 import bld_mtx_fly
+from ..count_matrix._bld_atac_mtx import bld_mtx_fly
 #from ..count_matrix._bld_met_mtx import methylation_level, write_not_sparse_meth, extract_methylation, extract_feature_names
 #from ..count_matrix._bld_met_mtx import read_meth_fileCG, read_meth_fileCH, read_meth_file, filter_and_average_features
 #from ..count_matrix._bld_met_mtx import filter_and_average_features_chrm, prep_methlevels, write_list, write_methlevel
