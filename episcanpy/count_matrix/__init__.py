a5111fb925e0cf1e23ec38275aa526e24cb3d799
Anna Danese <anna.danese@helmholtz-muenchen.de>
2021-03-23 13:18:18 +0100

    Update __init__.py

--- episcanpy/count_matrix/__init__.py
+++ episcanpy/count_matrix/__init__.py
@@ -1,4 +1,5 @@
 from ..count_matrix._features import load_features, make_windows, size_feature_norm, plot_size_features, name_features
+from ..count_matrix._features import load_features_gff, load_features_gtf
 from ..count_matrix._load_input_file import read_cyt_summary
 from ..count_matrix._bld_met_mtx import build_count_mtx
 from ..count_matrix._atac_mtx import bld_atac_mtx, save_sparse_mtx
