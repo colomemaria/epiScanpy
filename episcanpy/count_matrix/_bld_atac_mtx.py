94440da09bac849ef6d467348c0f1e7d092e82f3
Patrick Hanel <patrick_hanel@hotmail.com>
2022-11-30 16:13:12 +0100

    fixed feature sorting issue in count matrix fns

--- episcanpy/count_matrix/_bld_atac_mtx.py
+++ episcanpy/count_matrix/_bld_atac_mtx.py
@@ -282,6 +282,8 @@ def peak_mtx(fragments_file,
     names = ["chr", "start", "stop"]
     # cellranger peak files contain comments. So added comment parameter to skip them.
     features = pd.read_csv(peak_file, sep="\t", header=None, usecols=[0, 1, 2], names=names, comment='#')
+    features["chr"] = features.chr.astype(str)
+
     features.index = features.apply(lambda row: "_".join([str(val) for val in row]), axis=1)
 
     if normalized_peak_size:
@@ -334,6 +336,7 @@ def gene_activity_mtx(fragments_file,
 
     names = ["chr", "source", "type", "start", "stop", "score", "strand", "frame", "attribute"]
     features = pd.read_csv(gtf_file, sep="\t", header=None, comment="#", names=names)
+    features["chr"] = features.chr.astype(str)
 
     features = features[features.type == "gene"]
 
@@ -394,6 +397,8 @@ def window_mtx(fragments_file,
     features = [["chr{}".format(chrom), *window[:-1]] for chrom, windows in features.items() for window in windows]
 
     features = pd.DataFrame(features, columns=["chr", "start", "stop"])
+    features["chr"] = features.chr.astype(str)
+
     features.index = features.apply(lambda row: "_".join([str(val) for val in row]), axis=1)
 
     features.sort_values(by=["chr", "start", "stop"], key=lambda col: col if col.dtype == np.int64 else col.str.lower(), inplace=True)
