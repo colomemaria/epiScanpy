746932544da51217d7e873bea0a90d345c0e298d
Patrick Hanel <47063786+HAL9032@users.noreply.github.com>
2024-01-30 10:29:52 +0100

    moved binary_motif_mtx fn to enrichment analysis

--- episcanpy/tools/_motif_search.py
+++ episcanpy/tools/_motif_search.py
@@ -183,13 +183,3 @@ class MotifSearcher(multiprocessing.Process):
         self.q.put(results)
 
         self.sem.release()
-
-
-def binary_motif_mtx(adata):
-
-    motif_presence_mtx = np.zeros((len(adata.uns["motif_search"]["tf_motifs"]), adata.n_vars))
-
-    for tfbs in adata.uns["motif_search"]["results"]:
-        motif_presence_mtx[tfbs["motif_idx"], tfbs["feature_idx"]] = 1
-
-    adata.uns["motif_search"]["motif_presence_mtx"] = motif_presence_mtx
